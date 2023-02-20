8f5a38be7957ee41d76fbdb62e0f41861822ece2
DanJackson02 <dannyjjackson2002@gmail.com>
2023-02-20 15:50:50 +0000

    Part one and part two attempted. WORK IN PROGRESS

--- server.js
+++ server.js
@@ -6,7 +6,25 @@ const {sequelize} = require("./db")
 const port = 3000;
 
 //TODO
+/*
+1. In the `server.js` file, create a new endpoint for when the browser makes a `GET` request to http://localhost:3000/musicians.
+2. The endpoint will need to fetch these musicians from the database. 
+Have a look at the Sequelize Model's `findAll()` method from last week to help you with this. 
+3. Send the musicians as a JSON Response (`response.json()`).
+*/
 
+app.get('/Musician', (request, response) => {
+    Musician.findAll().then(Musician => {
+      response.json(Musician);
+    });
+  });
+  app.get('/Musician/:id', (req, res) => {
+    const musicianId = req.params.id;
+    Musician.findByPk(musicianId).then(Musician => {
+      res.json(Musician);
+    });
+  });
+  
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
